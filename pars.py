4cd5e86fe1c29095272f5656da68461888bba279
Den <den@localhost.localdomain>
2018-02-15 14:13:51 +0400

    nanana

---
+++ pars.py
@@ -0,0 +1,15 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+def main():
+    #https://coinmarketcap.com/all/views/all
+    all_links = []
+
+if __name__ == '__main__':
+    main()
